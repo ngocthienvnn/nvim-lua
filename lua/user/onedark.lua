35803a117a903b99b7060dbbfd0fb6bc98bbfd1c
thien <ngocthienvnn@gmail.com>
2022-08-12 21:17:03 +0700

    refactor colorscheme

--- lua/user/onedark.lua
+++ lua/user/onedark.lua
@@ -4,19 +4,15 @@ if not status_ok then
 end
 local bg = vim.opt.background:get()
 if (bg == 'light') then
-  print 'thien - light'
   onedark.setup {
     highlights = {
+      Normal = {bg='#f9f9f9'},
       TSPunctBracket = {fg='#ce0000'},
       TSFunction = {fg='#3361c3'}
     }}
 end
 if (bg == 'dark') then
-
   onedark.setup {
-    style = 'light',
-    toggle_style_key = "<Leader>qq", -- Default keybinding to toggle,
     highlights = {
-      TSKeyword = {fmt='none', fg='red'},
     }}
 end
