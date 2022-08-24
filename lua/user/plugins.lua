4fb51cc02d603e5b742fed6e0c3165421ffa25d4
thien <ngocthienvnn@gmail.com>
2022-08-24 13:20:08 +0700

    add neoscroll plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -141,6 +141,11 @@ return packer.startup(function(use)
   use 'mfussenegger/nvim-dap'
   use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
 
+  -- Scrolling
+
+  use {
+    'karb94/neoscroll.nvim'
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
