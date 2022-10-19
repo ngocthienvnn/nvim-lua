7d4990c4586c889cf4061b22d086e64ca97a0252
thien <ngocthienvnn@gmail.com>
2022-10-19 11:36:45 +0700

    adjust tresitter hightlight

--- lua/user/onedark.lua
+++ lua/user/onedark.lua
@@ -13,26 +13,26 @@ if (bg == 'light') then
       Normal = {bg='$_bg'},
       SignColumn = {bg='$_bg'},
       EndOfBuffer = {bg='$_bg'},
-      TSPunctBracket = {fg='#ce0000'},
-      TSFunction = {fg='#3361c3', fmt='bold'},
-      TSMethod = {fg='#3361c3', fmt='bold'},
-      TSString = {fg='#40813f'},
-      TSComment = {fg='#818187'},
+      ["@punctuation.bracket"] = {fg='#ce0000'},
+      ["@function"] = {fg='#3361c3', fmt='bold'},
+      ["@method"] = {fg='#3361c3', fmt='bold'},
+      ["@string"] = {fg='#40813f'},
+      ["@comment"] = {fg='#818187'},
       Pmenu = {bg='#dedede'},
       Search = {bg='#dadada', fg='NONE'},
       Visual = {bg='#dfdfdf'},
-      TSProperty = {fg='#b20245'},
-      -- MatchParen = {bg= '$_bg', fmt='bold', fg='black'}
+      ["@property"] = {fg='#b20245'},
+      MatchParen = {bg= '$_bg', fmt='bold', fg='black'}
     }}
 end
 if (bg == 'dark') then
   onedark.setup {
     style = 'darker',
     highlights = {
-      TSConstructor = {fg='#b69456'},
-      TSPunctBracket = {fg='#d45b5b'},
-      TSOperator = {fg='#d45b5b'},
-      -- MatchParen = {bg= '#1f2329', fmt='bold', fg='#f945c0'},
+      ["@constructor"] = {fg='#b69456'},
+      ["@punctuation.bracket"] = {fg='#d45b5b'},
+      ["@operator"] = {fg='#d45b5b'},
+      MatchParen = {bg= '#1f2329', fmt='bold', fg='#f945c0'},
       Search = {bg='#464a54', fg='NONE'}
     }
   }
