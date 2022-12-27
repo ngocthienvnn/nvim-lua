b0903d1132a46b473e152e8d4242286552cb45d0
thien <ngocthienvnn@gmail.com>
2022-12-27 23:06:37 +0700

    add autoclose tags

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -160,6 +160,11 @@ return packer.startup(function(use)
   -- use {"onsails/lspkind.nvim"}
 
   use 'eandrju/cellular-automaton.nvim' 
+  use { "windwp/nvim-ts-autotag",
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
