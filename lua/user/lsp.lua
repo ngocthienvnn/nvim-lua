0e20ebeb9572b214625e6a2868634a5d4221ee31
thien <ngocthienvnn@gmail.com>
2023-03-03 16:00:44 +0700

    add typescript plugin

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -92,11 +92,23 @@ lspconfig['intelephense'].setup {
 }
 
 -- tsserver
-lspconfig['tsserver'].setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  update_in_insert = false,
-}
+-- lspconfig['tsserver'].setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   update_in_insert = false,
+-- }
+require("typescript").setup({
+  disable_commands = false, -- prevent the plugin from creating Vim commands
+  debug = false, -- enable debug logging for commands
+  go_to_source_definition = {
+    fallback = true, -- fall back to standard LSP definition on failure
+  },
+  server = { -- pass options to lspconfig's setup method
+    on_attach = on_attach,
+    capabilities = capabilities,
+    update_in_insert = false,
+  },
+})
 
 -- luasnip setup
 local luasnip = require 'luasnip'
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -166,6 +166,8 @@ return packer.startup(function(use)
     end
   }
 
+  use 'jose-elias-alvarez/typescript.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
