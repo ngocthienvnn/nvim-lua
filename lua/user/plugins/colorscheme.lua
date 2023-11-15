93c7a5485cf24f8267f89217d2de805745043d1c
ngocthien <ngocthienvnn@gmail.com>
2023-11-16 00:04:38 +0700

    customize nightfox theme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,22 +1,38 @@
 return { "EdenEast/nightfox.nvim",
-config = function()
--- Specify the file path
-local file_path = os.getenv( "HOME" ) .."/.nvim_kitty/nvim.lua"
+  config = function()
+    -- Specify the file path
+    local file_path = os.getenv( "HOME" ) .."/.nvim_kitty/nvim.lua"
 
--- Try to load the file
-local success, config = pcall(dofile, file_path)
+    -- Try to load the file
+    local success, config = pcall(dofile, file_path)
 
--- Check if loading was successful
-if success then
-    -- Access the "colorscheme" value
-    local colorscheme = config["colorscheme"]
-    vim.cmd("colorscheme "..colorscheme)
+    -- Check if loading was successful
+    if success then
+      -- Access the "colorscheme" value
+      local colorscheme = config["colorscheme"]
+      require("nightfox").setup({
+        palettes = {
+          nightfox = {
+            bg1 = "#161f2b", -- Black background
+            fg1="#b8baba"
+          },
+        },
+        groups = {
+          nightfox = {
+            ['@operator'] = {fg='#859900'},
+            ['@punctuation.delimiter'] = {fg='#859900'},
+            ['@punctuation.bracket'] = {fg='#ce4c57'},
+          }
+        },
+      });
+      vim.cmd("colorscheme "..colorscheme)
+      -- Print the result (you can replace this with any further processing)
+    else
+      -- Handle the case when the file is not found
+      print("File not found. Setting colorscheme to empty.")
+      -- Print or use the empty colorscheme as needed
+    end
     -- Print the result (you can replace this with any further processing)
-else
-    -- Handle the case when the file is not found
-    print("File not found. Setting colorscheme to empty.")
-    -- Print or use the empty colorscheme as needed
-end
--- Print the result (you can replace this with any further processing)
-end
+  end,
+
 }
--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -1,6 +1,5 @@
 return { "lukas-reineke/indent-blankline.nvim", main = "ibl",
   config=function ()
-
     local ibl = require("ibl")
     ibl.setup({
       scope = {
