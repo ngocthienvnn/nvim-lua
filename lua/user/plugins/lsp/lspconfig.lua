264e44715c1b40d1ac7947dad4392cdec959eb22
thien <ngocthienvnn@gmail.com>
2023-11-16 11:11:27 +0700

    add telescopoe

--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -23,7 +23,8 @@ return {
       keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
       keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations      
+      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
       keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
       keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
       keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
@@ -33,7 +34,7 @@ return {
       keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
       keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
       keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-      keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
       keymap.set('n', '<space>f', function()
         vim.lsp.buf.format { async = true }
       end, opts)
