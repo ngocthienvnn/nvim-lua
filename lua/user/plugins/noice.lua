62754f494cccc2045bd586f4fe4c6886378da843
thien <ngocthienvnn@gmail.com>
2023-11-19 21:13:05 +0700

    update noice

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -11,5 +11,19 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+    },
+  config = function ()
+    require("noice").setup({
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      },
+    })
+  end
 }
