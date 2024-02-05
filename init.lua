245c7cb920a86e629cc11f00874bf9a6c9d7bebc
ngocthien <ngocthienvnn@gmail.com>
2024-02-05 13:50:53 +0700

    add custom parser for blade

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 require "user.core.init"
 require "user.lazy"
+
+vim.filetype.add({
+  pattern = {
+    ['.*%.blade%.php'] = 'blade',
+  },
+})
--- lua/user/plugins/nvim-treesiter.lua
+++ lua/user/plugins/nvim-treesiter.lua
@@ -11,6 +11,17 @@ return {
       -- import nvim-treesitter plugin
       local treesitter = require("nvim-treesitter.configs")
 
+      -- custom parser for BLADE
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = {"src/parser.c"},
+          branch = "main",
+        },
+        filetype = "blade"
+      }
+
       -- configure treesitter
       treesitter.setup({ -- enable syntax highlighting
         highlight = {
